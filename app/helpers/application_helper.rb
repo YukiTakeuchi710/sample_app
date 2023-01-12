a2391fdb26f5918d76c0763a65121673326de81c
ytrb8823 <y-takeuchi@rhizome-e.com>
2023-01-12 13:29:22 +0900

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  # ページごとの完全なタイトルを返す
+  # full_title
+  # 引数が入ってない場合は''として扱う
+  def full_title(page_title = '')
+    # base_title 基本タイトル
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
