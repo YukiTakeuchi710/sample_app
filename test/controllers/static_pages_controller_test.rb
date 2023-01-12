639a10d1733f58cc2194a37e37563cb10dd08174
ytrb8823 <y-takeuchi@rhizome-e.com>
2023-01-12 17:18:20 +0900

    Push Transport Test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -6,7 +6,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
 
-
+  # ルートの検証コードです。
   test "should get home" do
     get root_path
     assert_response :success
