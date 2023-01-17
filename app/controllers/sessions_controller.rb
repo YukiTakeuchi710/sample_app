7b27743f2a57000bfb953008241f4122afe46ad9
ytrb8823 <y-takeuchi@rhizome-e.com>
2023-01-17 10:31:26 +0900

    Finish user edit, index and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,24 +5,22 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user &.authenticate(params[:session][:password])
+      forwarding_url = session[:forwarding_url]
       reset_session      # ログインの直前に必ずこれを書くこと
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       log_in user
-      redirect_to user
+
+      redirect_to forwarding_url || user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       # エラーメッセージを作成する
       render 'new', status: :unprocessable_entity
     end
-
-    def destroy
-      log_out if logged_in?
-
-      redirect_to root_url, status: :see_other
-    end
   end
-
 
   def destroy
+      log_out if logged_in?
+      redirect_to root_url, status: :see_other
   end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,20 @@
 class UsersController < ApplicationController
+  # アクションを実行する前に、ログインしているユーザーでないと不可能
+  # まだほかのユーザでも編集はできる
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+
+  # 本人のユーザ
+
   def new
     @user = User.new
 
+  end
+
+  # ユーザー一覧
+  def index
+    @users = User.paginate(page: params[:page])
   end
 
   def show
@@ -20,9 +33,52 @@ class UsersController < ApplicationController
       render 'new', status: :unprocessable_entity
     end
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit', status: :unprocessable_entity
+    end
+  end
+
+  # ユーザ削除
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url, status: :see_other
+  end
+
   private
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+  # beforeフィルタ
+
+  # ログイン済みユーザーかどうか確認
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url, status: :see_other
+      end
+    end
+
+  # 正しいユーザーかどうか確認
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url, status: :see_other) unless current_user?(@user)
+    end
+
+    # 管理者かどうか確認
+    def admin_user
+      redirect_to(root_url, status: :see_other) unless current_user.admin?
     end
 end
