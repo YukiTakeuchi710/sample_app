6c43fe2df8d0e5c17559a16206ed24b14d28fa7d
ytrb8823 <y-takeuchi@rhizome-e.com>
2023-01-17 14:02:09 +0900

    Add account activation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,6 +17,10 @@ Rails.application.configure do
   # Enable server timing
   config.server_timing = true
 
+  host = 'localhost:3000'
+  # localhostで開発している場合は以下をお使いください
+  config.action_mailer.default_url_options = { host: host, protocol: 'http' }
+
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join("tmp/caching-dev.txt").exist?
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -83,8 +83,7 @@ class RememberingTest < UsersLogin
 
   test "login with remembering" do
     log_in_as(@user, remember_me: '1')
-    #assert_not cookies[:remember_token].blank?
-    #assert_equal cookies[:remember_token], assigns(:user).sess
+    assert_not cookies[:remember_token].blank?
   end
 
   test "login without remembering" do
