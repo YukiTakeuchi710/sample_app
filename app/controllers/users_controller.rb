db60cf9d62015d1f19dc072aaddd3e0cd187dcbd
ytrb8823 <y-takeuchi@rhizome-e.com>
2023-01-18 11:22:19 +0900

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,8 +19,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
     redirect_to root_url and return unless @user.activated?
-
   end
 
   def create
@@ -56,28 +56,21 @@ class UsersController < ApplicationController
   end
 
   private
+
     def user_params
       params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
-    end
-  # beforeフィルタ
-
-  # ログイン済みユーザーかどうか確認
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url, status: :see_other
-      end
+                                 :password_confirmation)
     end
 
-  # 正しいユーザーかどうか確認
+    # beforeフィルター
+
+    # 正しいユーザーかどうかを確認
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url, status: :see_other) unless current_user?(@user)
     end
 
-    # 管理者かどうか確認
+    # 管理者かどうかを確認
     def admin_user
       redirect_to(root_url, status: :see_other) unless current_user.admin?
     end
