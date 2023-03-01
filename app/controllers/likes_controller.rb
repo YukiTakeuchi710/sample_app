07966659504e680862a90da2b9c596254ea96db7
ytrb8823 <y-takeuchi@rhizome-e.com>
2023-03-01 16:05:48 +0900

    like and bad

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,19 +1,16 @@
 
 class LikesController < ApplicationController
   before_action :logged_in_user
-  def index
-    @microposts = Like.find_by(micropost_id: params[:id])
-  end
-
   def create
     @micropost = Micropost.find(params[:micropost_id])
     unless @micropost.liked?(current_user)
       if @micropost.like(current_user)
         respond_to do |format|
           format.html{ redirect_to microposts_path(@micropost)}
-          # format.html { redirect_to request.referrer || root_url }
           format.turbo_stream
         end
+      else
+
       end
     end
   end
@@ -24,9 +21,9 @@ class LikesController < ApplicationController
       @micropost.unlike(current_user)
       respond_to do |format|
         format.html{ redirect_to microposts_path(@micropost)}
-        # format.html { redirect_to request.referrer || root_url }
         format.turbo_stream
       end
     end
   end
+
 end
